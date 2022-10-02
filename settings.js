b83e912dcf266851f5b30bc7919f508809fefe1b
Shekah2 <114748952+Shekah2@users.noreply.github.com>
2022-10-02 18:43:25 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,10 +19,10 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
+global.owner = ['94717635804']
+global.premium = ['94717635804']
+global.ownernomer = '94717635804'
+global.ownername = 'kushan buddhimal'
 global.botname = 'Gojo-Satoru'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
